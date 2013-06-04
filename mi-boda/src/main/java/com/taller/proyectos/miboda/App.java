60306c7aca1bfb6498ac457005a1bc4ff98e58d1
javier <javier@jetchegaray>
2013-06-03 23:44:46 -0300

    estructura

---
+++ mi-boda/src/main/java/com/taller/proyectos/miboda/App.java
@@ -0,0 +1,13 @@
+package com.taller.proyectos.miboda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
