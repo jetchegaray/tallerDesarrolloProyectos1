565b1c551ed1c19781876b01082ab4258895f1eb
Juan Manuel Barreneche <snipperme@gmail.com>
2013-06-15 18:47:36 -0300

    Cleanup registration form

---
+++ app/views/helper/bootstrap/package.scala
@@ -0,0 +1,26 @@
+package views.html.helper
+
+// import views.html.helper.FieldConstructor
+
+/**
+ * Contains template helpers, for example for generating HTML forms.
+ */
+package object bootstrap {
+
+  /**
+   * Twitter twitterBootstrap input structure.
+   *
+   * {{{
+   * <dl>
+   *   <dt><label for="username"></dt>
+   *   <dd><input type="text" name="username" id="username"></dd>
+   *   <dd class="error">This field is required!</dd>
+   *   <dd class="info">Required field.</dd>
+   * </dl>
+   * }}}
+   */
+  implicit val defaultField = new FieldConstructor {
+    def apply(elts: FieldElements) = bootstrapFieldConstructor(elts)
+  }
+
+}
