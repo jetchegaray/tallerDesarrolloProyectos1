77eafd480a23d6ed6f923519390558b19608c014
Juan Manuel Barreneche <snipperme@gmail.com>
2013-07-20 23:50:10 -0300

    Include tasks for civil event

--- app/model/domain/events/Civil.java
+++ app/model/domain/events/Civil.java
@@ -1,11 +1,14 @@
 package model.domain.events;
 
 import model.domain.Event;
+import model.domain.tasks.FakeTask;
 
 public class Civil extends Event {
 
 	public Civil() {
 		super("Civil");
+		tasks.add(new FakeTask("Reservar registro civil", "101"));
+		tasks.add(new FakeTask("Enviar invitaciones", "102"));
 	}
 
 }
