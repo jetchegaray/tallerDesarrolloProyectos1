ccee62ea4c7b5554a764759a970d470b4b7eb4b8
Juan Manuel Barreneche <snipperme@gmail.com>
2013-07-28 18:47:18 -0300

    Add ability to add expenses manualy

--- app/controllers/WeddingController.java
+++ app/controllers/WeddingController.java
@@ -8,8 +8,10 @@ import org.bson.types.ObjectId;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import play.mvc.With;
 
 @Security.Authenticated(WeddingAuthenticator.class)
+@With(WeddingContext.class)
 public class WeddingController extends Controller {
 
 	public static Wedding currentWedding() {
