fe113f2ff26894a21cc6cf300df67af019500d32
javier <javier@jetchegaray.(none)>
2013-08-10 19:40:07 -0300

    agrego linkeo de novedades a la base

--- app/controllers/HiringController.java
+++ app/controllers/HiringController.java
@@ -17,7 +17,8 @@ public class HiringController extends WeddingController {
 	public static Form<Hiring> hiringForm = Form.form(Hiring.class);
 
 	public static Result hiring() {
-		return ok(views.html.hirings.hiring.render(getHirings()));
+		Hiring hiring = HiringDAO.getHiringDAO().find().get();
+		return ok(views.html.hirings.hiring.render(getHirings(),hiring));
 	}
 	
 	public static List<Hiring> getHirings(){
@@ -35,31 +36,8 @@ public class HiringController extends WeddingController {
 	//FIXME cualquiera ¡¡¡
 	public static Result getConversation(String name){
 		Hiring hiring = HiringDAO.getHiringDAO().findOne("name",name);
-	//	hiringForm.fill(hiring).get().getName()
 		return ok(views.html.hirings.conversation.render(hiring));
 	}
 	
 
-//	public static Result createExpense(String expenseType) {
-//		Form<Expense> filledForm = hiringForm.bindFromRequest();
-//		Wedding wedding = currentWedding();
-//
-//		if (!filledForm.hasErrors()) {
-//			if (expenseType == null) {
-//				expenseType = new DynamicForm().bindFromRequest().get("type");
-//			}
-//
-//			Budgetable budgeteable = selectBudget(wedding, expenseType);
-//			Expense expense = filledForm.get();
-//			budgeteable.addExpense(expense);
-//			WeddingDAO.instance.save(wedding);
-//
-//			return redirect(parent(expenseType));
-//
-//		} else {
-//
-//			return badRequest(newExpense.render(filledForm, expenseType));
-//
-//		}
-//	}
 }
